70dac04837ab454340a54801d8e6defa6abe0ac4
Luey890 <114149842+Luey890@users.noreply.github.com>
2026-05-27 09:05:23 +1000

    Commit1
    Fixed retrieval errors in relation to the database and text files including 'visitor_log.txt'.

---
+++ The_Unsecure_PWA-2026-main/user_management.py
@@ -0,0 +1,71 @@
+import sqlite3 as sql
+import time
+import random
+import os
+
+
+def insertUser(username, password, DoB):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "database_files", "database.db")
+    con = sql.connect(db_path)
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
+        (username, password, DoB),
+    )
+    con.commit()
+    con.close()
+
+
+def retrieveUsers(username, password):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "database_files", "database.db")
+    con = sql.connect(db_path)
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    if cur.fetchone() == None:
+        con.close()
+        return False
+    else:
+        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
+        # Plain text log of visitor count as requested by Unsecure PWA management
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        v_path = os.path.join(BASE_DIR, "visitor_log.txt")
+        with open(v_path) as file:
+            number = int(file.read().strip())
+            number += 1
+        with open(v_path, 'w') as file:
+            file.write(str(number))
+        # Simulate response time of heavy app for testing purposes
+        time.sleep(random.randint(80, 90) / 1000)
+        if cur.fetchone() == None:
+            con.close()
+            return False
+        else:
+            con.close()
+            return True
+
+
+def insertFeedback(feedback):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "database_files", "database.db")
+    con = sql.connect(db_path)
+    cur = con.cursor()
+    cur.execute(f"INSERT INTO feedback (feedback) VALUES ('{feedback}')")
+    con.commit()
+    con.close()
+
+
+def listFeedback():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "database_files", "database.db")
+    con = sql.connect(db_path)
+    cur = con.cursor()
+    data = cur.execute("SELECT * FROM feedback").fetchall()
+    con.close()
+    #f = open("templates/partials/success_feedback.html", "w")
+    for row in data:
+        f.write("<p>\n")
+        f.write(f"{row[1]}\n")
+        f.write("</p>\n")
+    f.close()
